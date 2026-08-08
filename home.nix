9e190c96464651c492f590edd0d90d0bfe1f9e7b
ards447 <193798239+ards447@users.noreply.github.com>
2026-08-09 02:04:36 +0530

    Manage Waybar config

--- home.nix
+++ home.nix
@@ -69,6 +69,8 @@
     # '';
   };
 
+  xdg.configFile."waybar".source = ./dotfiles/waybar;
+
   # Home Manager can also manage your environment variables through
   # 'home.sessionVariables'. These will be explicitly sourced when using a
   # shell provided by Home Manager. If you don't want to manage your shell
